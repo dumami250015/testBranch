148db1e5d4b1d150de1fd4d864c8fab9914df224
dumami250015 <khoivotri@gmail.com>
2025-10-22 07:31:05 +0700

    commit

---
+++ newBranch.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define ii pair <int, int>
+#define iii pair<ii, int>
+#define vii vector<ii>
+#define X first
+#define Y second
+using namespace std;
+
+const int oo = 1e18;
+const double eps = 0.00001;
+const int mod = 1e9 + 7;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
